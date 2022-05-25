65db02225cb84dd47640a64fe76a5b50639cc98c
Alex Platt <alja.platt@gmail.com>
2022-05-25 16:54:35 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const searchBtn = document.querySelector('.search-btn');
+const userInputEl = document.querySelector('input');
+const movieContainer = document.querySelector('.movie-container');
+
+const renderMovies = movieArr => {
+	movieArr.map(movie => {
+		// create div to hold data for each movie
+		const movieDiv = document.createElement('div');
+		movieDiv.classList.add('movieDiv');
+		const textDiv = document.createElement('div');
+		textDiv.classList.add('textDiv');
+		// append movieDiv to HTML
+		movieContainer.append(movieDiv);
+
+		// create element for each property we want to show, add content & append to its div
+		//* poster
+		const posterEl = document.createElement('img');
+		posterEl.classList.add('poster');
+		const poster = movie.Poster;
+		// console.log(poster);
+		posterEl.src = poster;
+		movieDiv.append(posterEl);
+		//==========================================
+		//* title
+		const titleEl = document.createElement('h3');
+		const title = movie.Title;
+		titleEl.textContent = `${title}`;
+		textDiv.append(titleEl);
+		//==========================================
+		//* year
+		const yearEl = document.createElement('p');
+		const year = movie.Year;
+		yearEl.textContent = `${year}`;
+		textDiv.append(yearEl);
+		movieDiv.append(textDiv);
+		// const imdbID = movie.imdbID;
+		// console.log(title);
+	});
+};
+
+const fetchMovies = async _ => {
+	const userInput = userInputEl.value;
+	// console.log(userInput);
+
+	let res = await fetch(
+		`https://www.omdbapi.com/?s=${userInput.trim()}&apikey=7188dc6d`
+	);
+	let data = await res.json();
+	// console.log(data);
+	const movies = data.Search;
+	console.log(movies);
+	renderMovies(movies);
+};
+
+searchBtn.addEventListener('click', fetchMovies);

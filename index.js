8859328743f3b2103fdd03c430b22a861b4925d7
Alex Platt <alja.platt@gmail.com>
2022-05-26 08:40:14 +0100

    imdb data & search styling on search page

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const movieContainer = document.querySelector('.movie-container');
 
 const renderMovies = async movieArr => {
 	movieArr.map(async movie => {
+		//* imdb info
+		const imdbId = movie.imdbID;
+		let response = await fetch(
+			`https://www.omdbapi.com/?i=${imdbId}&apikey=7188dc6d`
+		);
+		let imdbData = await response.json();
+		console.log(imdbData);
+		//=========================================
 		// create div to hold data for each movie
 		const movieDiv = document.createElement('div');
 		movieDiv.classList.add('movieDiv');
@@ -23,24 +31,43 @@ const renderMovies = async movieArr => {
 		//==========================================
 		//* title
 		const titleEl = document.createElement('h3');
+		titleEl.classList.add('title');
 		const title = movie.Title;
 		titleEl.textContent = `${title}`;
 		textDiv.append(titleEl);
 		//==========================================
+		// //* rating
+		const ratingEl = document.createElement('span');
+		const rating = imdbData.Rated;
+		ratingEl.textContent = `${rating}`;
+		textDiv.append(ratingEl);
+		//=========================================
 		//* year
 		const yearEl = document.createElement('p');
-		const year = movie.Year;
+		yearEl.classList.add('margin-bottom');
+		const year = imdbData.Released;
 		yearEl.textContent = `${year}`;
 		textDiv.append(yearEl);
 		movieDiv.append(textDiv);
 		//=========================================
-		//* imdb info
-		const imdbId = movie.imdbID;
-		let response = await fetch(
-			`https://www.omdbapi.com/?i=${imdbId}&apikey=7188dc6d`
-		);
-		let imdbData = await response.json();
-		console.log(imdbData);
+		//* plot
+		const plotEl = document.createElement('p');
+		plotEl.classList.add('margin-bottom');
+		const plot = imdbData.Plot;
+		plotEl.textContent = `${plot}`;
+		textDiv.append(plotEl);
+		//========================================
+		//* runtime
+		const runtimeEl = document.createElement('p');
+		const runtime = imdbData.Runtime;
+		runtimeEl.textContent = `${runtime}`;
+		textDiv.append(runtimeEl);
+		//========================================
+		//* imdb rating
+		const imdbRatingEl = document.createElement('p');
+		const imdbRating = imdbData.imdbRating;
+		imdbRatingEl.textContent = `${imdbRating}`;
+		textDiv.append(imdbRatingEl);
 	});
 };
 
